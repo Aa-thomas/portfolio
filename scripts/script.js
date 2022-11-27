ee133b62d8e2f041348d648136a6f0a78531919f
Aa-thomas <aarthomas01@gmail.com>
2022-11-27 17:23:00 -0500

    add about section and styles, slight contact and header tweaks

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+//  Dropdown menu
+const menubtn = document.querySelector('i.menu');
+const xmark = document.querySelector('i.xmark');
+const dropdownContent = document.querySelector('.dropdown');
+
+menubtn.addEventListener('click', (e) => {
+	console.log(dropdownContent);
+	dropdownContent = 'block';
+});

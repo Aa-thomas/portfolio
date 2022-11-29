547543b8de981007657171295240328ee5dbae6d
Aa-thomas <aarthomas01@gmail.com>
2022-11-28 19:13:34 -0500

    complete fade/slide-in functionality

---
+++ scripts/int-observer.js
@@ -0,0 +1,47 @@
+const sections = document.querySelectorAll('.observe');
+
+const options = {
+	root: null,
+	threshold: 0,
+	rootMargin: '-150px',
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+	entries.forEach((entry) => {
+		if (!entry.isIntersecting) return;
+		console.log(entry.target);
+		observer.unobserve(entry.target);
+	});
+}, options);
+
+sections.forEach((section) => {
+	observer.observe(section);
+});
+
+// Fade-In
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+	threshold: 0,
+	rootMargin: '0px 0px -250px 0px',
+};
+
+const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+	entries.forEach((entry) => {
+		if (!entry.isIntersecting) return;
+		entry.target.classList.add('appear');
+		appearOnScroll.unobserve(entry.target);
+	});
+}, appearOptions);
+
+faders.forEach((fader) => {
+	appearOnScroll.observe(fader);
+});
+
+// Slide-In
+
+const sliders = document.querySelectorAll('.slide-in');
+
+sliders.forEach((slider) => {
+	appearOnScroll.observe(slider);
+});

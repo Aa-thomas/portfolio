39fbf608b334558af6f67dd92eee0f2f39d51b0d
Aa-thomas <aarthomas01@gmail.com>
2022-12-01 20:15:47 -0500

    edit dropdown menu

--- scripts/script.js
+++
@@ -1,9 +0,0 @@
-// //  Dropdown menu
-// const menubtn = document.querySelector('i.menu');
-// const xmark = document.querySelector('i.xmark');
-// const dropdownContent = document.querySelector('.dropdown');
-
-// menubtn.addEventListener('click', (e) => {
-// 	console.log(dropdownContent);
-// 	dropdownContent = 'block';
-// });
